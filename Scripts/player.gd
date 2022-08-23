0fe9d75747e14cace3a94a33fe313a5785698f83
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-23 20:53:50 +0300

    Added some basic code to test mouse motion

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,4 +4,7 @@ func _ready() -> void:
 	pass;
 	
 func _input(event: InputEvent) -> void:
-	pass;
+	var mouse_motion = event as InputEventMouseMotion;
+	
+	if(mouse_motion):
+		print("Mouse motion");
