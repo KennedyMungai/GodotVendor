6f95fc9ae6d26ee56e0d1eadd45fa4b33c2ecc56
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-23 21:07:44 +0300

    Added the export variable of mouse sensitivity and implemented it into the rotation code

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody
 
+export var _mouse_sensitivity: float = 0.05;
+
 func _ready() -> void:
 	pass;
 	
@@ -7,4 +9,4 @@ func _input(event: InputEvent) -> void:
 	var mouse_motion = event as InputEventMouseMotion;
 	
 	if(mouse_motion):
-		rotation_degrees.y -= mouse_motion.relative.x;
+		rotation_degrees.y -= mouse_motion.relative.x * _mouse_sensitivity;
