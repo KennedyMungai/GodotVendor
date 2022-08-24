4896e4bd68f0a739a15cbf687e59fc29533f76fb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-24 22:17:35 +0300

    Added sideways movement code and implemented the speed variable to the movement code

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,8 @@ extends KinematicBody
 
 export var _mouse_sensitivity: float = 0.05
 
+var speed: float = 5.0
+
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
@@ -9,17 +11,19 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var movement_vector: Vector3
 	var forward_movement: Vector3
+	var sideways_movement: Vector3
 	
 	if(Input.is_action_pressed("move_forward")):
 		forward_movement = -transform.basis.z
 	elif(Input.is_action_pressed("move_back")):
 		forward_movement = transform.basis.z
-	elif(Input.is_action_pressed("move_left")):
-		print("Moving left")
-	elif(Input.is_action_pressed("move_right")):
-		print("Moving right")
 		
-	movement_vector = forward_movement
+	if(Input.is_action_pressed("move_left")):
+		sideways_movement = -transform.basis.x
+	elif(Input.is_action_pressed("move_right")):
+		sideways_movement = transform.basis.x
+		
+	movement_vector = (forward_movement + sideways_movement) * speed
 	
 	move_and_slide(movement_vector)
 
