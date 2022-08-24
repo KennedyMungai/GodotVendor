756ebc543c48b68a2d0d1ddc61ca77f1bd69169d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-24 19:53:52 +0300

    Imported the camera and performed some rotations on iot

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody
 
-export var _mouse_sensitivity: float = 0.1
+onready var camera: Camera = get_node("Camera")
+
+export var _mouse_sensitivity: float = 0.05
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -9,5 +11,5 @@ func _input(event: InputEvent) -> void:
 	var mouse_motion = event as InputEventMouseMotion
 	
 	if(mouse_motion):
-		rotation_degrees.y -= mouse_motion.relative.x * _mouse_sensitivity
-		rotation_degrees.x -= mouse_motion.relative.y * _mouse_sensitivity
+		camera.rotation_degrees.y -= mouse_motion.relative.x * _mouse_sensitivity
+		camera.rotation_degrees.x -= mouse_motion.relative.y * _mouse_sensitivity
