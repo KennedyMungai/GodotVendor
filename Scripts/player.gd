eaa593e24b5370b2b73118b20f42b3666e6fc84d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-24 20:25:33 +0300

    Separated out the aimimg code and called it inside the input function

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,11 +7,13 @@ func _ready() -> void:
 	
 
 func _input(event: InputEvent) -> void:
+	aim(event)
+
+
+func aim(event: InputEvent) -> void:
 	var mouse_motion = event as InputEventMouseMotion
-	
 	if(mouse_motion):
 		rotation_degrees.y -= mouse_motion.relative.x * _mouse_sensitivity
 		var current_tilt:float=$Camera.rotation_degrees.x
 		current_tilt-= mouse_motion.relative.y * _mouse_sensitivity
 		$Camera.rotation_degrees.x = clamp(current_tilt, -90, 90)
-		
