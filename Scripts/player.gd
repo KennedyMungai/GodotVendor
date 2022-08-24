a302f4770366996ac7ebab982e06b95cd56c36d0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-24 19:47:34 +0300

    Added vertical movement for the camera

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,13 @@
 extends KinematicBody
 
-export var _mouse_sensitivity: float = 0.1;
+export var _mouse_sensitivity: float = 0.1
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func _input(event: InputEvent) -> void:
-	var mouse_motion = event as InputEventMouseMotion;
+	var mouse_motion = event as InputEventMouseMotion
 	
 	if(mouse_motion):
-		rotation_degrees.y -= mouse_motion.relative.x * _mouse_sensitivity;
+		rotation_degrees.y -= mouse_motion.relative.x * _mouse_sensitivity
+		rotation_degrees.x -= mouse_motion.relative.y * _mouse_sensitivity
