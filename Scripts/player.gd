9e2ada1992ed65bd1b045aba579949466bf796bb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-24 20:07:03 +0300

    Implemented a clamp for the horizontal camera rotation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -10,4 +10,7 @@ func _input(event: InputEvent) -> void:
 	
 	if(mouse_motion):
 		rotation_degrees.y -= mouse_motion.relative.x * _mouse_sensitivity
-		$Camera.rotation_degrees.x -= mouse_motion.relative.y * _mouse_sensitivity
+		var current_tilt:float=$Camera.rotation_degrees.x
+		current_tilt-= mouse_motion.relative.y * _mouse_sensitivity
+		$Camera.rotation_degrees.x = clamp(current_tilt, -90, 90)
+		
