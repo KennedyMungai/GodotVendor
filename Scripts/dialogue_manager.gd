a37ba10e00cf76a143be27f494dc7a3683cc6e5f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-28 11:49:25 +0300

    Refactored the code to get any issues the code has with scope

--- Scripts/dialogue_manager.gd
+++ Scripts/dialogue_manager.gd
@@ -1,10 +1,14 @@
 extends Control
 
 
+# Export variables
 export(NodePath) var _dialogue_text_path
+
+# Normal variables
+var dialogue_text:Label
 
 
 func _ready() -> void:
-	var dialogue_text: Label = get_node(_dialogue_text_path)
+	dialogue_text = get_node(_dialogue_text_path)
 	dialogue_text.text = "Welcome to the game. Dumb motherfucker!!!!"
 	pass
