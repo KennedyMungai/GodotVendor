776491ef642a70f5fc9fe315bdf5b7a6d81dd7cc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-28 11:51:36 +0300

    Refactored the code by setting the path to an omready variable

--- Scripts/dialogue_manager.gd
+++ Scripts/dialogue_manager.gd
@@ -4,11 +4,10 @@ extends Control
 # Export variables
 export(NodePath) var _dialogue_text_path
 
-# Normal variables
-var dialogue_text:Label
+# Onready variables
+onready var dialogue_text:Label = get_node(_dialogue_text_path)
 
 
 func _ready() -> void:
-	dialogue_text = get_node(_dialogue_text_path)
 	dialogue_text.text = "Welcome to the game. Dumb motherfucker!!!!"
 	pass
