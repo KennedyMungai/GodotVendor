395422ec460595d9ceec442f60d185df75e5c743
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-28 11:43:57 +0300

    Initilized the value of the label of te label in the dialogue ui

--- Scripts/dialogue_manager.gd
+++ Scripts/dialogue_manager.gd
@@ -6,4 +6,5 @@ export(NodePath) var _dialogue_text_path
 
 func _ready() -> void:
 	var dialogue_text: Label = get_node(_dialogue_text_path)
+	dialogue_text.text = "Welcome to the game. Dumb motherfucker!!!!"
 	pass
