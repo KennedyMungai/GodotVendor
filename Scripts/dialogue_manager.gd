5e5cb4efe5735eff7623960a6015c8bb56c9bdfc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-28 12:02:13 +0300

    refactored the code to use the onready variable in a single line with the proper type casting

--- Scripts/dialogue_manager.gd
+++ Scripts/dialogue_manager.gd
@@ -2,10 +2,9 @@ extends Control
 
 
 # Export variables
-export(NodePath) var _dialogue_text_path
+export(NodePath) onready var dialogue_text = get_node(dialogue_text) as Label
 
 # Onready variables
-onready var dialogue_text:Label = get_node(_dialogue_text_path)
 
 
 func _ready() -> void:
