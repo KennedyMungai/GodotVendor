fcaf245cb3dbba08e72ebb41a211de3080606ea4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-28 12:35:07 +0300

    Added a subroutine to update the dialogue slide

--- Scripts/dialogue_manager.gd
+++ Scripts/dialogue_manager.gd
@@ -8,7 +8,20 @@ export(Resource) onready var current_dialogue=current_dialogue as Dialogue
 
 # Onready variables
 
+# Normal variables
+var current_slide_index:int=0
+
 
 func _ready() -> void:
-	dialogue_text.text = current_dialogue.dialogue_slides[0]
 	avatar.texture=current_dialogue.avatar_texture
+	show_slide()
+
+
+func _input(event: InputEvent) -> void:
+	if(Input.is_action_just_pressed("advance_slide")):
+		current_slide_index+=1
+		
+
+# A function for showing slide
+func show_slide() -> void:
+	dialogue_text.text = current_dialogue.dialogue_slides[current_slide_index]
