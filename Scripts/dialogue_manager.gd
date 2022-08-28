75238ece28dd179ee88c71914ef8e2525c8fd95f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-28 15:24:46 +0300

    Added some code to togglethe dialogue UI for testing purposes

--- Scripts/dialogue_manager.gd
+++ Scripts/dialogue_manager.gd
@@ -15,6 +15,9 @@ var current_slide_index:int=0
 func _ready() -> void:
 	avatar.texture=current_dialogue.avatar_texture
 	show_slide()
+	self.visible=false
+	
+	GameEvents.connect("dialog_initiated", self, "on_dialog_initiated")
 
 
 func _input(event: InputEvent) -> void:
@@ -29,3 +32,9 @@ func _input(event: InputEvent) -> void:
 # A function for showing slide
 func show_slide() -> void:
 	dialogue_text.text = current_dialogue.dialogue_slides[current_slide_index]
+
+
+# The function called when there is dialogue initiated
+func on_dialog_initiated() -> void:
+	print("Dialogue has been initiated")
+	self.visible=true
