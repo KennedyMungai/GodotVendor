29598f69c6a2a1229fc6888b4c9462ac1bb7a2ee
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-28 14:06:15 +0300

    Added some tests for the dialog trigger in the scene

---
+++ Scenes/dialogue_trigger.gd
@@ -0,0 +1,13 @@
+extends Area
+
+
+
+
+func _on_dialogue_trigger_body_entered(body: Node) -> void:
+	print("Entered")
+	
+
+
+
+func _on_dialogue_trigger_body_exited(body: Node) -> void:
+	print("Exited")
