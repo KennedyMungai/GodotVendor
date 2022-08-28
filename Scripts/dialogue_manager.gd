f44fc33667717b7d56d26741bbaef231603d962a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-28 12:19:18 +0300

    Updated the reference to the dialogue intro tres file

--- Scripts/dialogue_manager.gd
+++ Scripts/dialogue_manager.gd
@@ -4,11 +4,11 @@ extends Control
 # Export variables
 export(NodePath) onready var dialogue_text = get_node(dialogue_text) as Label
 export(NodePath) onready var avatar = get_node(avatar) as TextureRect
-export(Resource) var current_dialogue_tres
+export(Resource) onready var current_dialogue=current_dialogue as Dialogue
 
 # Onready variables
 
 
 func _ready() -> void:
-	dialogue_text.text = current_dialogue_tres.dialogue_slides[0]
+	dialogue_text.text = current_dialogue.dialogue_slides[0]
 #	avatar.texture=
