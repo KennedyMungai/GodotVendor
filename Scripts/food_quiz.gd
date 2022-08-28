5f5fbe972fd30c1bd13e6e15eba1c7d5860d56a9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-28 14:49:12 +0300

    Added type casting to the dialogue resource

--- Scripts/food_quiz.gd
+++ Scripts/food_quiz.gd
@@ -1,8 +1,8 @@
 extends Spatial
 
 
-export(Resource) var dialogue
-
+export(Resource) var dialogue=dialogue as Dialogue
+	
 
 func _on_dialogue_trigger_body_entered(body: Node) -> void:
 	print("Body entered")
