4bfec7a3f528e8878c91ac14ff9e68294337fd44
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-28 12:06:56 +0300

    Added an onready var reference to the code

--- Scripts/dialogue_manager.gd
+++ Scripts/dialogue_manager.gd
@@ -3,6 +3,7 @@ extends Control
 
 # Export variables
 export(NodePath) onready var dialogue_text = get_node(dialogue_text) as Label
+export(NodePath) onready var avatar = get_node(avatar) as TextureRect
 
 # Onready variables
 
