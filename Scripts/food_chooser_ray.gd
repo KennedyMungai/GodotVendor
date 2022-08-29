1d53fbb566e095eb015d37f3a212813f62c384f9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-29 20:40:40 +0300

    Updated the function call

--- Scripts/food_chooser_ray.gd
+++ Scripts/food_chooser_ray.gd
@@ -5,6 +5,6 @@ func _input(event: InputEvent) -> void:
 	var collided_area: Area = get_collider()
 	
 	if(collided_area):
-		if((collided_area.get_parent()).is_class("Food")):
+		if((collided_area.get_parent()) is Food):
 			print("Miracle")
 			collided_area.get_parent().on_mouse_entered()
