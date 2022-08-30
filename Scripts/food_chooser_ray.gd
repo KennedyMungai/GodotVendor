95d96871721368273cc8bd4c22bd6b0b779910ce
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-30 20:43:21 +0300

    Updated the code format

--- Scripts/food_chooser_ray.gd
+++ Scripts/food_chooser_ray.gd
@@ -10,5 +10,5 @@ func _input(event: InputEvent) -> void:
 		if((collided_area.get_parent()) is Food):
 			if not is_mousing_over:
 				is_mousing_over=true
-			collided_area.get_parent().on_mouse_entered(collided_area.get_parent())
-			GameEvents.emit_signal("food_moused_over", collided_area.get_parent())
+				collided_area.get_parent().on_mouse_entered(collided_area.get_parent())
+				GameEvents.emit_signal("food_moused_over", collided_area.get_parent())
