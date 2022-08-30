7f12abbfd7d925502122ef70992024e0e31301aa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-30 20:49:34 +0300

    Updated the code

--- Scripts/food_chooser_ray.gd
+++ Scripts/food_chooser_ray.gd
@@ -6,12 +6,11 @@ var is_mousing_over:bool=false
 func _input(event: InputEvent) -> void:
 	var collided_area: Area = get_collider()
 	
-	if(collided_area):
-		if((collided_area.get_parent()) is Food):
-			if not is_mousing_over:
-				is_mousing_over=true
-				collided_area.get_parent().on_mouse_entered(collided_area.get_parent())
-				GameEvents.emit_signal("food_moused_over", collided_area.get_parent())
+	if((collided_area) and (collided_area.get_parent()) is Food):
+		if not is_mousing_over:
+			is_mousing_over=true
+			collided_area.get_parent().on_mouse_entered(collided_area.get_parent())
+			GameEvents.emit_signal("food_moused_over", collided_area.get_parent())
 	elif(is_mousing_over):
 		is_mousing_over=false
 		GameEvents.emit_signal("food_moused_out")
