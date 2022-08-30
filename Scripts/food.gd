a29d4f6db1ba257be9a47e0f835b95e62fc94c40
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-30 20:13:37 +0300

    Connected the food moused over signal in the ready function which calls the on_mouse_entered function

--- Scripts/food.gd
+++ Scripts/food.gd
@@ -2,5 +2,10 @@ extends Spatial
 
 class_name Food
 
+
+func _ready() -> void:
+	GameEvents.connect("food_moused_over", self, "on_mouse_entered")
+
+
 func on_mouse_entered() -> void:
 	print("Mouse entered")
