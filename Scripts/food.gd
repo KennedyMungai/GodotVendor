97e1f1223028ffa6816e342ddbc1cf22d2d8cb6d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-30 20:30:56 +0300

    Created a function that handles scenarios when the mouse has stopped hovering over the specific items

--- Scripts/food.gd
+++ Scripts/food.gd
@@ -7,6 +7,12 @@ func _ready() -> void:
 	GameEvents.connect("food_moused_over", self, "on_mouse_entered")
 
 
+# Function gets called when a mouse hovers over it
 func on_mouse_entered(food_item: Food) -> void:
 	if(food_item == self):
 		$SpotLight.visible = true
+
+
+# Function get called when the mouse stops hovering ove it
+func on_mouse_out() -> void:
+	$SpotLight.visible=false
