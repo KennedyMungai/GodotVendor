fbf3f83ac9b892c7c07e1efb46f756ebff297243
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-30 20:39:45 +0300

    Created the is_mousing_over boolean variable and set it to true when the raycast interacts with the fruit

--- Scripts/food_chooser_ray.gd
+++ Scripts/food_chooser_ray.gd
@@ -1,10 +1,13 @@
 extends RayCast
 
+var is_mousing_over:bool=false
+
 
 func _input(event: InputEvent) -> void:
 	var collided_area: Area = get_collider()
 	
 	if(collided_area):
 		if((collided_area.get_parent()) is Food):
+			is_mousing_over=true
 			collided_area.get_parent().on_mouse_entered(collided_area.get_parent())
 			GameEvents.emit_signal("food_moused_over", collided_area.get_parent())
